ec3cd2ee006ac0aef618c7599ca228ceca15e9ef
saitejasri1 <saitejasri10@gmail.com>
2025-03-12 23:57:12 -0700

    Updates

--- index.js
+++ index.js
@@ -52,6 +52,7 @@ import { fileURLToPath } from "url";
 var __filename = fileURLToPath(import.meta.url);
 var __dirname = dirname(__filename);
 var vite_config_default = defineConfig({
+  base: "./",
   plugins: [
     react(),
     runtimeErrorOverlay(),
